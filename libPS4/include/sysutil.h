3555cb1b6bcee25efa16c974c74e35a7c6128064
stooged <stooged@users.noreply.github.com>
2018-07-03 23:47:06 +0800

    r4

--- libPS4/include/sysutil.h
+++ libPS4/include/sysutil.h
@@ -3,8 +3,8 @@
 #include "libc.h"
 
 extern int (*sceSysUtilSendSystemNotificationWithText)(int messageType, char* message);
+extern int (*sceSystemServiceLaunchWebBrowser)(const char *uri, void *);
 
 void initSysUtil(void);
-
 void systemMessage(char* msg);
-
+void openBrowser(char* uri);
--- libPS4/source/sysutil.c
+++ libPS4/source/sysutil.c
@@ -1,14 +1,15 @@
 #include "kernel.h"
 #include "module.h"
-
 #include "sysutil.h"
 
 int (*sceSysUtilSendSystemNotificationWithText)(int messageType, char* message);
+int (*sceSystemServiceLaunchWebBrowser)(const char *uri, void *);
 
 void initSysUtil(void) {
 	int sysUtilHandle = sceKernelLoadStartModule("/system/common/lib/libSceSysUtil.sprx", 0, NULL, 0, 0, 0);
-
+	int libSceSystemService = sceKernelLoadStartModule("/system/common/lib/libSceSystemService.sprx", 0, NULL, 0, 0, 0);
 	RESOLVE(sysUtilHandle, sceSysUtilSendSystemNotificationWithText);
+	RESOLVE(libSceSystemService, sceSystemServiceLaunchWebBrowser);
 }
 
 void systemMessage(char* msg) {
@@ -17,3 +18,6 @@ void systemMessage(char* msg) {
  	sceSysUtilSendSystemNotificationWithText(0x81, buffer);
 }
 
+void openBrowser(char* uri) {
+ 	sceSystemServiceLaunchWebBrowser(uri, NULL);
+}
