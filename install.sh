5a8674953088195f1480283ee6462b91e14c4b4e
Al Azif <alazif@miskatonic.io>
2019-01-03 19:09:23 -0800

    Single script install

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Prerequisites
+apt-get update
+apt-get -y upgrade
+apt-get -y install make clang gcc llvm binutils
+
+# Clone Repo
+git clone https://github.com/Scene-Collective/ps4-payload-sdk ~/PS4-SDK
+
+# Build SDK
+pushd ~/PS4-SDK
+make
+popd
+
+# Add to paths
+echo "export PS4SDK=~/PS4-SDK" | tee -a ~/.bash.rc
+source ~/.bashrc
