5ec5f854f41e5f64ece114ac40984507491c9e08
Al-Azif <alazif@miskatonic.io>
2020-12-26 02:28:40 -0800

    Add rootcheck for update/install

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@
 
 # NOTE: Assuming Ubuntu 20.04
 
-# Prerequisites
-apt-get update
-apt-get -y install \
-  binutils=2.34-6ubuntu1 \
-  gcc=4:9.3.0-1ubuntu2 \
-  make=4.2.1-1.2
+# Install prerequisites if root
+if [ "$EUID" -ne 0 ]; then
+  echo "Not root, skipping update and install"
+else
+  apt-get update
+  apt-get -y install \
+    binutils=2.34-6ubuntu1 \
+    gcc=4:9.3.0-1ubuntu2 \
+    make=4.2.1-1.2
+fi
 
 # Delete directory if it exists and make empty directory
 rm -rf ~/PS4SDK || true
-mkdir -p ~/PS4SDK
+mkdir -p ~/PS4SD
 
 # Build SDK
 pushd libPS4
