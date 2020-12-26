40a78e73e577e4508ed3536c59b0e2f1d3901d88
Al-Azif <alazif@miskatonic.io>
2020-12-26 02:10:03 -0800

    Revert

--- install.sh
+++ install.sh
@@ -9,14 +9,21 @@ apt-get -y install \
   gcc=4:9.3.0-1ubuntu2 \
   make=4.2.1-1.2
 
+# Delete directory if it exists and make empty directory
+rm -rf ~/PS4SDK || true
+mkdir -p ~/PS4SDK
+
 # Build SDK
 pushd libPS4
 make
 popd
 
+# Move compiled SDK
+mv libPS4 ~/PS4SDK/libPS4
+
 # Clear path from .bashrc if it's there
 sed -i "s/^\s*export PS4SDK=.*//gm" ~/.bashrc
 
 # Add to paths
-echo "export PS4SDK=$(pwd)/PS4SDK" | tee -a ~/.bashrc
-export PS4SDK=$(pwd)/PS4SDK
+echo "export PS4SDK=~/PS4SDK" | tee -a ~/.bashrc
+export PS4SDK=~/PS4SDK
