953dd44653023d8937ebe4280c0d7f00fffbb25f
Al-Azif <alazif@miskatonic.io>
2020-12-29 07:15:09 -0800

    Shrink muh macros

--- libPS4/include/payload_utils.h
+++ libPS4/include/payload_utils.h
@@ -114,94 +114,88 @@ static inline __attribute__((always_inline)) void writeCr0(uint64_t cr0) {
                    : "memory");
 }
 
-#define copyout_macro(x) {                                                      \
+#define copyout_macro(x)                                                        \
   kernel_base = &((uint8_t *)__readmsr(0xC0000082))[-K ## x ## _XFAST_SYSCALL]; \
-  copyout = (void *)(kernel_base + K ## x ## _COPYOUT);                         \
-}
+  copyout = (void *)(kernel_base + K ## x ## _COPYOUT);
 
-#define jailbreak_macro(x) {                                                    \
+#define jailbreak_macro(x)                                                      \
   kernel_base = &((uint8_t *)__readmsr(0xC0000082))[-K ## x ## _XFAST_SYSCALL]; \
   kernel_ptr = (uint8_t *)kernel_base;                                          \
   prison0 = (void **)&kernel_ptr[K ## x ## _PRISON_0];                          \
-  rootvnode = (void **)&kernel_ptr[K ## x ## _ROOTVNODE];                       \
-}
+  rootvnode = (void **)&kernel_ptr[K ## x ## _ROOTVNODE];
 
-#define mmap_macro(x) {                                                         \
+#define mmap_macro(x)                                                           \
   kernel_base = &((uint8_t *)__readmsr(0xC0000082))[-K ## x ## _XFAST_SYSCALL]; \
   kernel_ptr = (uint8_t *)kernel_base;                                          \
   mmap_patch_1 = &kernel_ptr[K ## x ## _MMAP_SELF_1];                           \
   mmap_patch_2 = &kernel_ptr[K ## x ## _MMAP_SELF_2];                           \
-  mmap_patch_3 = &kernel_ptr[K ## x ## _MMAP_SELF_3];                           \
-}
+  mmap_patch_3 = &kernel_ptr[K ## x ## _MMAP_SELF_3];
 
-#define aslr_macro(x) {                                                         \
+#define aslr_macro(x)                                                           \
   kernel_base = &((uint8_t *)__readmsr(0xC0000082))[-K ## x ## _XFAST_SYSCALL]; \
   kernel_ptr = (uint8_t *)kernel_base;                                          \
-  aslr_patch = &kernel_ptr[K ## x ## _DISABLE_ASLR];                            \
-}
+  aslr_patch = &kernel_ptr[K ## x ## _DISABLE_ASLR];
 
-#define kclock_macro(x) {                                                                     \
+#define kclock_macro(x)                                                                       \
   kernel_base = &((uint8_t *)__readmsr(0xC0000082))[-K ## x ## _XFAST_SYSCALL];               \
   if (atoi(#x) >= 450) {                                                                      \
     sceSblSrtcClearTimeDifference = (void *)(kernel_base + K ## x ## _CLEAR_TIME_DIFFERENCE); \
     sceSblSrtcClearTimeDifference(15);                                                        \
   }                                                                                           \
-  sceSblSrtcSetTime = (void *)(kernel_base + K ## x ## _SET_TIME);                            \
-}
+  sceSblSrtcSetTime = (void *)(kernel_base + K ## x ## _SET_TIME);
 
-#define enable_browser_macro(x) {                                             \
+#define enable_browser_macro(x)                                                 \
   kernel_base = &((uint8_t *)__readmsr(0xC0000082))[-K ## x ## _XFAST_SYSCALL]; \
-  sceRegMgrSetInt = (void *)(kernel_base + K ## x ## _REG_MGR_SET_INT);         \
-}
+  sceRegMgrSetInt = (void *)(kernel_base + K ## x ## _REG_MGR_SET_INT);
 
-#define caseentry(id, macro) { \
-  case id:                     \
-    macro(id);                 \
-    break;                     \
-}
+#define caseentry(id, macro) \
+  case id:                   \
+    macro(id);               \
+    break;
 
-#define build_kpayload(id, macro)    \
-  switch(id) {                       \
-    caseentry(350, macro);           \
-    caseentry(355, macro);           \
-    caseentry(370, macro);           \
-    caseentry(400, macro);           \
-    caseentry(401, macro);           \
-    caseentry(405, macro);           \
-    caseentry(406, macro);           \
-    caseentry(407, macro);           \
-    caseentry(450, macro);           \
-    caseentry(455, macro);           \
-    caseentry(470, macro);           \
-    caseentry(471, macro);           \
-    caseentry(472, macro);           \
-    caseentry(473, macro);           \
-    caseentry(474, macro);           \
-    caseentry(500, macro);           \
-    caseentry(501, macro);           \
-    caseentry(503, macro);           \
-    caseentry(505, macro);           \
-    caseentry(507, macro);           \
-    caseentry(550, macro);           \
-    caseentry(553, macro);           \
-    caseentry(555, macro);           \
-    caseentry(556, macro);           \
-    caseentry(600, macro);           \
-    caseentry(602, macro);           \
-    caseentry(620, macro);           \
-    caseentry(650, macro);           \
-    caseentry(651, macro);           \
-    caseentry(670, macro);           \
-    caseentry(671, macro);           \
-    caseentry(672, macro);           \
-    caseentry(700, macro);           \
-    caseentry(701, macro);           \
-    caseentry(702, macro);           \
-    caseentry(750, macro);           \
-    caseentry(751, macro);           \
-    caseentry(755, macro);           \
-    default:                         \
-      __asm__("ret");                \
+#define build_kpayload(id, macro) \
+  switch(id) {                    \
+    caseentry(350, macro);        \
+    caseentry(355, macro);        \
+    caseentry(370, macro);        \
+    caseentry(400, macro);        \
+    caseentry(401, macro);        \
+    caseentry(405, macro);        \
+    caseentry(406, macro);        \
+    caseentry(407, macro);        \
+    caseentry(450, macro);        \
+    caseentry(455, macro);        \
+    caseentry(470, macro);        \
+    caseentry(471, macro);        \
+    caseentry(472, macro);        \
+    caseentry(473, macro);        \
+    caseentry(474, macro);        \
+    caseentry(500, macro);        \
+    caseentry(501, macro);        \
+    caseentry(503, macro);        \
+    caseentry(505, macro);        \
+    caseentry(507, macro);        \
+    caseentry(550, macro);        \
+    caseentry(553, macro);        \
+    caseentry(555, macro);        \
+    caseentry(556, macro);        \
+    caseentry(600, macro);        \
+    caseentry(602, macro);        \
+    caseentry(620, macro);        \
+    caseentry(650, macro);        \
+    caseentry(651, macro);        \
+    caseentry(670, macro);        \
+    caseentry(671, macro);        \
+    caseentry(672, macro);        \
+    caseentry(700, macro);        \
+    caseentry(701, macro);        \
+    caseentry(702, macro);        \
+    caseentry(750, macro);        \
+    caseentry(751, macro);        \
+    caseentry(755, macro);        \
+    default:                      \
+      __asm__("ret");             \
+      /* raise(SIGSEGV); */       \
   }
 
 int is_fw_spoofed();
