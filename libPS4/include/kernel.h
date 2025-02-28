15ace2e3ab2d8fa2c4f73fe59bea5f84731edf93
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2025-02-28 05:05:22 -0800

    Update kernel.h

--- libPS4/include/kernel.h
+++ libPS4/include/kernel.h
@@ -89,6 +89,7 @@ extern int (*sceKernelGetSystemSwVersion)(SceFwInfo *fw_info);
 extern uint32_t (*sceKernelGetCpuTemperature)(uint32_t *);
 
 extern uint32_t (*sceKernelGetIdPs)(void *);
+extern uint32_t (*sceKernelGetOpenPsId)(void *);
 extern uint32_t (*sceKernelGetOpenPsIdForSystem)(void *);
 
 int kill(int pid, int signum);
